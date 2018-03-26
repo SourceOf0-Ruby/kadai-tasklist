f8f89faa293f43f6a70fd6586ab7c0dd40bc97a6
Source of 0 <SourceOf0@gmail.com>
2018-03-26 05:31:01 +0000

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+state_list = ["新規", "未着手", "着手", "遅延", "保留", "取消", "完了"];
+(1..10).each do |num|
+  state_list.each do |s|
+    Task.create(status: s, content: 'test content ' + num.to_s);
+  end
+end
