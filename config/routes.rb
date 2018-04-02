f6130b20ccf61e53adbc14aa8849bede7c1c26a7
Source of 0 <SourceOf0@gmail.com>
2018-04-02 09:03:35 +0000

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,24 @@
 Rails.application.routes.draw do
   
-  root to: 'tasks#index'
+  # トップページ
+  root to: 'tasks#index';
   
-  resources :tasks
+  # ログイン周り
+  get    'login' , to: 'sessions#new';
+  post   'login' , to: 'sessions#create';
+  delete 'logout', to: 'sessions#destroy';
+
+
+  # ユーザ周り
+  
+  # /signup へのアクセスでuserが新規作成できるようにする
+  get 'signup', to: 'users#new';
+  
+  # index, new, destroyは無し
+  resources :users, only: [:show, :create, :edit, :update];
+
+
+  # タスク周り
+  resources :tasks;
   
 end
